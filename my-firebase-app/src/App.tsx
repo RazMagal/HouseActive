bb8e6bf411a3a8d74cc1d5e37813302c42036926
Raz Magal <loi201loi@cs.huji.ac.il>
2025-04-05 18:55:37 +0300

    Removed logos and their .svg files Added SignIn and SignOut

--- my-firebase-app/src/App.tsx
+++ my-firebase-app/src/App.tsx
@@ -1,45 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SignInButton from './components/SignInButton'
+import { auth } from './assets/config/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [user] = useAuthState(auth)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-      <h1>Vite + React</h1>
-      
+    <>      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lolz it's a counter from the template I used to start this project
         </p>
       </div>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
       <div>
         <h1>Welcome to HouseActive</h1>
+        {!user &&  (
         <SignInButton />
+        )}
       </div>
+      {user && (
+      <div>
+        <h1><p> {auth.currentUser?.displayName} </p></h1>
+        <img src={auth.currentUser?.photoURL || ""} width="100" height="100" alt="Profile" />
+        <p> {auth.currentUser?.email} </p>
+        <p> your user uid is: {auth.currentUser?.uid} </p>
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+      </div>
+      )}
     </>
-
   )
 }
 
-export default App
+export default App 
